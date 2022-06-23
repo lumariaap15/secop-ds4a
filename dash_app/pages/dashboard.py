329e8d896aeb36d88557affab6cae0cc8992b14a
Luisa Alzate <lumariaap15@gmail.com>
2022-06-22 20:25:50 -0500

    adding graph layout

--- dash_app/pages/dashboard.py
+++ dash_app/pages/dashboard.py
@@ -1,12 +1,55 @@
 ### Import Packages ###
-from dash import dcc
+from dash import dcc, html
 from dataframes.data import fig
+import dash_bootstrap_components as dbc
 
 ### Import Dash Instance ###
 #from app import app
 
 ### Page 1 Layout and Callback ###
-layout = dcc.Graph(
+graph = dcc.Graph(
         id='example-graph',
         figure=fig
     )
+
+layout = html.Div([
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dbc.Col([
+                    graph
+                ], width=3),
+                dbc.Col([
+                    graph
+                ], width=3),
+                dbc.Col([
+                    graph
+                ], width=3),
+                dbc.Col([
+                    graph
+                ], width=3),
+            ], align='center'), 
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    graph
+                ], width=3),
+                dbc.Col([
+                    graph
+                ], width=3),
+                dbc.Col([
+                    graph
+                ], width=6),
+            ], align='center'), 
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    graph
+                ], width=9),
+                dbc.Col([
+                    graph
+                ], width=3),
+            ], align='center'),      
+        ]), color = 'light'
+    )
+])
